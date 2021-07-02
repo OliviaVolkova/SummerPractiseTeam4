cd4895f684f904c4a53df0673d197f0c84a961db
HotBlender <Danil@DESKTOP-CLT0SUH>
2021-07-02 19:12:50 +0300

    added bottom menu nav

--- app/src/main/java/com/itis/englishgram/BottomSingleActivity.kt
+++ app/src/main/java/com/itis/englishgram/BottomSingleActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class BottomSingleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_bottom_single)
         //
     }
 }
