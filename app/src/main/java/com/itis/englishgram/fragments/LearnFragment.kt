1ca2ab8324d0e5784388f15151e0b068b4e34d34
Mansur <ahmatowmansur@gmail.com>
2021-07-06 18:34:13 +0300

    Learn готов. Вроде

--- app/src/main/java/com/itis/englishgram/fragments/LearnFragment.kt
+++ app/src/main/java/com/itis/englishgram/fragments/LearnFragment.kt
@@ -1,8 +1,59 @@
 package com.itis.englishgram.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.itis.englishgram.R
+import com.itis.englishgram.models.Word
+import com.itis.englishgram.models.WordsLists
 
-class LearnFragment : Fragment(R.layout.fragment_learn) {
+class LearnFragment : Fragment(R.layout.fragment_learn)
+{
+    lateinit var wordView : TextView
+    lateinit var definitionView : TextView
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListeners()
+
+        wordView = view.findViewById(R.id.wordView)
+        definitionView = view.findViewById(R.id.definitionView)
+
+        WordsLists.addExampleWords()
+
+        takeNewWord()
+    }
+
+    lateinit var currentWord : Word
+
+    fun takeNewWord()
+    {
+        currentWord = WordsLists.takeRandomUnknown()
+        wordView.text = currentWord.spelling
+        definitionView.text = currentWord.definition
+    }
+
+    fun initListeners()
+    {
+        val knowButton = view?.findViewById<Button>(R.id.IKnowButton)
+        knowButton?.setOnClickListener(){
+            WordsLists.moveFromUnknownToKnown(currentWord)
+            takeNewWord()
+        }
+
+        val learnButton = view?.findViewById<Button>(R.id.ToLearnButton)
+        learnButton?.setOnClickListener(){
+            WordsLists.moveWordFromUnknownToLearning(currentWord)
+            takeNewWord()
+        }
+
+        val skipButton = view?.findViewById<Button>(R.id.SkipButton)
+        skipButton?.setOnClickListener(){
+            takeNewWord()
+        }
+
+    }
 }
---
+++ app/src/main/java/com/itis/englishgram/models/WordsLists.kt
@@ -0,0 +1,97 @@
+package com.itis.englishgram.models
+
+import kotlin.random.Random
+
+
+//тут система слов для логики. Не для отображения в Vocabulary (там другая система).
+class WordsLists {
+    companion object
+    {
+        private val unknownList  = ArrayList<Word>()
+        private val learningList = ArrayList<Word>()
+        private val knownList = ArrayList<Word>()
+        private val random = Random
+
+        fun moveWordFromUnknownToLearning(word: Word)
+        {
+            if(unknownList.contains(word)) {
+                learningList.add(word)
+                unknownList.remove(word)
+            }
+        }
+        fun moveWordFromLearningToKnow(word: Word)
+        {
+            if(learningList.contains(word)) {
+                knownList.add(word)
+                learningList.remove(word)
+            }
+        }
+        fun moveFromUnknownToKnown(word: Word)
+        {
+            if(unknownList.contains(word)) {
+                knownList.add(word)
+                unknownList.remove(word)
+            }
+        }
+
+        fun unknownSize(): Int{
+            return unknownList.size
+        }
+        fun learningSize(): Int{
+            return learningList.size
+        }
+        fun knownSize() : Int{
+            return knownList.size
+        }
+
+        fun takeRandomUnknown() : Word
+        {
+            if(unknownSize()>0) {
+                val index = random.nextInt(unknownSize())
+                return unknownList[index]
+            }
+            else
+                return Word("Out of words", "We have no new words for you")
+        }
+
+        fun takeRandom() : Word
+        {
+            val sizesSum = unknownSize() + learningSize() + knownSize()
+            var index = random.nextInt(sizesSum)
+            if(index < unknownSize())
+                return unknownList[index]
+            index -= unknownSize()
+            if(index < learningSize())
+                return learningList[index]
+            index -= learningSize()
+            return knownList[index]
+        }
+
+
+        fun addWordToUnknown(word : Word)
+        {
+            unknownList.add(word)
+        }
+        fun addWordToUnknown(words : ArrayList<Word>)
+        {
+            unknownList.addAll(words)
+        }
+
+        fun addExampleWords()
+        {
+            //https://www.vocabulary.com/lists/52473
+            val words = arrayListOf<Word>(
+                Word("chair", "a seat for one person that has a back, usually four legs, and sometimes two arms"),
+                Word("pen", "a long, thin object used for writing or drawing with ink"),
+                Word("consider", "deem to be"),
+                Word("minute", "infinitely or immeasurably small"),
+                Word("accord", "concurrence of opinion"),
+                Word("evident", "clearly revealed to the mind or the senses or judgment"),
+                Word("practice", "a customary way of operation or behavior"),
+                Word("intend", "have in mind as a purpose"),
+                Word("concern", "something that interests you because it is important")
+            )
+            addWordToUnknown(words)
+        }
+    }
+}
